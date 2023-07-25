e33bc7341729591b067ddd57734970e6f254ac71
Minh <blueminh789.coding@gmail.com>
2023-07-25 18:48:16 +0700

    consume login context and added log in/out buttons

--- src/components/NavigationBar/index.jsx
+++ src/components/NavigationBar/index.jsx
@@ -1,7 +1,11 @@
-import {Container, Nav, Navbar} from "react-bootstrap";
+import {Container, Nav, Navbar,  Button} from "react-bootstrap";
 import "./navbar.css"
+import { LoginContext } from "../../App";
+import { useContext, useEffect} from "react";
 
 export default function NavigationBar() {
+    const [loggedIn, setLoggedIn] = useContext(LoginContext);
+    useEffect(() => { console.log("ahhhh", loggedIn);}, [])
     return (
         <>
         <Navbar collapseOnSelect expand="lg" className="header-footer" sticky="top">
@@ -18,6 +22,32 @@ export default function NavigationBar() {
                     <Nav className="me-auto">
                         <Nav.Link href="/calculation">Calculation Tool</Nav.Link>
                         <Nav.Link href="/validation">Validation Tool</Nav.Link>
+                    </Nav>
+                    <Nav>
+                    {loggedIn ? 
+                        <div>
+                            <Button
+                                className="primary-button btn-primary shadow-none btn-outline-dark button-navbar-padding"
+                            >
+                                Log out
+                            </Button>
+                        </div>
+                    : (<div>
+                            <Button
+                                className="primary-button btn-primary shadow-none btn-outline-dark button-navbar-padding"
+                                href="/register"
+                            >
+                                Register
+                            </Button>
+                            <Button
+                                className="primary-button btn-primary shadow-none btn-outline-dark button-navbar-padding"
+                                href="/login"
+                            >
+                                Log In
+                            </Button>
+                        </div>
+                        ) 
+                    }
                     </Nav>
                 </Navbar.Collapse>
             </Container>
