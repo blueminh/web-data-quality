52be62ea3cb35f81ac6f4856aae9fce7797accd2
Minh <blueminh789.coding@gmail.com>
2023-07-30 23:40:59 +0700

    finished the suggestions

--- src/components/validationTool/ValidationBoardPage/index.jsx
+++ src/components/validationTool/ValidationBoardPage/index.jsx
@@ -10,19 +10,18 @@ import { Chart as ChartJS, ArcElement, Tooltip, Legend, CategoryScale,
     BarElement,
     Title,} from 'chart.js';
 
-ChartJS.register(ArcElement, Tooltip, Legend);
-ChartJS.register(
-    CategoryScale,
-    LinearScale,
-    BarElement,
-    Title,
-    Tooltip,
-    Legend)
-console.log("test")
+    ChartJS.register(ArcElement, Tooltip, Legend);
+    ChartJS.register(
+        CategoryScale,
+        LinearScale,
+        BarElement,
+        Title,
+        Tooltip,
+        Legend)
+    console.log("test")
 
 export default function ValidationDashBoard() {
 
-    const [fileNames, setFileNames] = useState([]);
     const [basicInformation, setBasicInformation] = useState({
         nameOfDatabase: "KPMG",
         dateStart: "xxxx-xx-xx",
@@ -48,7 +47,7 @@ export default function ValidationDashBoard() {
         }]
       });
 
-      const [setOfDataInBar, setSetOfDataInBar] = useState([{
+    const [setOfDataInBar, setSetOfDataInBar] = useState([{
         fileName: 'Transaction.xls',
         text: 5,
         numeric: 7,
@@ -61,28 +60,23 @@ export default function ValidationDashBoard() {
         numeric: 5,
         datetime: 3,
         numberOfLines: 3
-      }])
+      }]
+    )
 
-      const dataForBarChart = (dataInBar) => {
+    const dataForBarChart = (dataInBar) => {
         return {
             labels: ["Text field", "Numeric"],
             datasets: [
                 {
-                  label: dataInBar.fileName,
-                  data: [dataInBar.text, dataInBar.numeric],
-                  backgroundColor: 'rgba(255, 99, 132, 0.5)',
+                    label: dataInBar.fileName,
+                    data: [dataInBar.text, dataInBar.numeric],
+                    backgroundColor: 'rgba(255, 99, 132, 0.5)',
                 },
                 
-              ], 
+                ], 
         }
-      }
-
-
-   const handleFileChange = (event) => {
-        const files = event.target.files;
-        console.log(files)
-        setFileNames(Array.from(files).map(extractFileName => extractFileName.name))
     }
+
 
     const updateStartDate = (event) => { setBasicInformation((prev) => ({
         ...prev,
@@ -100,42 +94,80 @@ export default function ValidationDashBoard() {
         }, 500);
         console.log('page to reload')
     }
-
     
-    const tableNames = [
-        'customers',
-        'orders',
-        'products',
-        'employees',
-        'suppliers',
-        // Add more table names as needed
-    ];
-
-    const [value, setValue] = useState('');
-    const [suggestions, setSuggestions] = useState([]);
-
-    const getSuggestions = (inputValue) => {
+    
+    // suggestion logic for database
+    const [databaseNameOptions, setDatabaseNameOptions] = useState([
+        'KPMG',
+        'KPMG 2'
+    ]);
+    const [databaseName, setDatabaseName] = useState('');
+    const [databaseSuggestions, setDatabaseSuggestions] = useState([]);
+    const getDatabaseSuggestions = (inputValue) => {
         const inputValueLowerCase = inputValue.trim().toLowerCase();
-        const filteredSuggestions = tableNames.filter((name) =>
+        const filteredSuggestions = databaseNameOptions.filter((name) =>
             name.toLowerCase().includes(inputValueLowerCase)
         );
         return filteredSuggestions;
     };
 
-    const onSuggestionsFetchRequested = ({ value }) => {
-        setSuggestions(getSuggestions(value));  
+    // suggestion logic for table name
+    // list of suggested table names
+    const [tableNameOptions, setTableNameOptions] = useState([
+        'customers',
+        'orders',
+        'products',
+        'employees',
+        'suppliers',
+    ]);
+    const [tableName, setTableName] = useState('');
+    const [tableSuggestions, setTableSuggestions] = useState([]);
+    const getTableSuggestions = (inputValue) => {
+        const inputValueLowerCase = inputValue.trim().toLowerCase();
+        const filteredSuggestions = tableNameOptions.filter((name) =>
+            name.toLowerCase().includes(inputValueLowerCase)
+        );
+        return filteredSuggestions;
     };
 
-    const onSuggestionSelected = (event, { suggestion }) => {
-        setValue(suggestion);
+    // suggestion logic for excluded table name
+    const [excludedTableNameOptions, setExcludedTableNameOptions] = useState([
+        'customers',
+        'orders',
+        'products',
+        'employees',
+        'suppliers',
+    ]);
+    const [excludedTableName, setExcludedTableName] = useState('');
+    const [excludedTableSuggestions, setExcludedTableSuggestions] = useState([]);
+    const getExcludedTableSuggestions = (inputValue) => {
+        const inputValueLowerCase = inputValue.trim().toLowerCase();
+        const filteredSuggestions = excludedTableNameOptions.filter((name) =>
+            name.toLowerCase().includes(inputValueLowerCase)
+        );
+        return filteredSuggestions;
     };
 
-    const inputProps = {
-        placeholder: 'Start typing to search...',
-        value,
-        onChange: (event, { newValue }) => {
-            setValue(newValue);
-        },
+
+    // handle file upload
+    const [csvData, setCsvData] = useState(null);
+    const handleFileInputChange = (event) => {
+        const file = event.target.files[0];
+        const reader = new FileReader();
+    
+        reader.onload = (e) => {
+          const csvContent = e.target.result;
+          setCsvData(csvContent);
+        };
+    
+        reader.readAsText(file); // Read the file as text
+    };    
+
+    // handle chossing separation symbol
+    const separationSymbols = [',', ';', '|'];
+    const [separationSymbol, setSeparationSymbol] = useState(',')
+    const handleSeparationSymbolChange = (selectedSymbol) => {
+        setSeparationSymbol(selectedSymbol)
     };
 
     return (
@@ -145,85 +177,99 @@ export default function ValidationDashBoard() {
                 <Col>
                     <Stack gap={3}>
                         <div className="form-input">
-                            <Form.Label>Select Database</Form.Label>
+                            <Form.Label>Cơ sở dữ liệu</Form.Label>
                             <Autosuggest
-                                suggestions={suggestions}
-                                onSuggestionsFetchRequested={onSuggestionsFetchRequested}
-                                onSuggestionsClearRequested={() => setSuggestions([])}
-                                onSuggestionSelected={onSuggestionSelected}
+                                suggestions={databaseSuggestions}
+                                onSuggestionsFetchRequested={({ value }) => {
+                                    setDatabaseSuggestions(getDatabaseSuggestions(value, ));  
+                                }}
+                                onSuggestionsClearRequested={() => setDatabaseSuggestions([])}
+                                onSuggestionSelected={(event, { suggestion }) => {
+                                    setDatabaseName(suggestion);
+                                }}
                                 getSuggestionValue={(suggestion) => suggestion}
                                 renderSuggestion={(suggestion) => <span>{suggestion}</span>}
-                                inputProps={inputProps}
+                                inputProps={{
+                                    placeholder: 'Start typing to search...',
+                                    value: databaseName,
+                                    onChange: (event, { newValue }) => {
+                                        setDatabaseName(newValue);
+                                    },
+                                }}
                             />
                         </div>
                         <div className="form-input">
                             <Form.Label>Tên bảng cần kiểm tra</Form.Label>
                             <Autosuggest
-                                suggestions={suggestions}
-                                onSuggestionsFetchRequested={onSuggestionsFetchRequested}
-                                onSuggestionsClearRequested={() => setSuggestions([])}
-                                onSuggestionSelected={onSuggestionSelected}
+                                suggestions={tableSuggestions}
+                                onSuggestionsFetchRequested={({ value }) => {
+                                    setTableSuggestions(getTableSuggestions(value, ));  
+                                }}
+                                onSuggestionsClearRequested={() => setTableSuggestions([])}
+                                onSuggestionSelected={(event, { suggestion }) => {
+                                    setTableName(suggestion);
+                                }}
                                 getSuggestionValue={(suggestion) => suggestion}
                                 renderSuggestion={(suggestion) => <span>{suggestion}</span>}
-                                inputProps={inputProps}
+                                inputProps={{
+                                    placeholder: 'Start typing to search...',
+                                    value: tableName,
+                                    onChange: (event, { newValue }) => {
+                                        setTableName(newValue);
+                                    },
+                                }}
                             />
                         </div>
                         <div className="form-input">
-                            <Form.Label>Tên bảng loại trừ</Form.Label>
+                            <Form.Label>Tên bảng cần loại trừ</Form.Label>
                             <Autosuggest
-                                suggestions={suggestions}
-                                onSuggestionsFetchRequested={onSuggestionsFetchRequested}
-                                onSuggestionsClearRequested={() => setSuggestions([])}
-                                onSuggestionSelected={onSuggestionSelected}
+                                suggestions={excludedTableSuggestions}
+                                onSuggestionsFetchRequested={({ value }) => {
+                                    setExcludedTableSuggestions(getExcludedTableSuggestions(value, ));  
+                                }}
+                                onSuggestionsClearRequested={() => setExcludedTableSuggestions([])}
+                                onSuggestionSelected={(event, { suggestion }) => {
+                                    setExcludedTableName(suggestion);
+                                }}
                                 getSuggestionValue={(suggestion) => suggestion}
                                 renderSuggestion={(suggestion) => <span>{suggestion}</span>}
-                                inputProps={inputProps}
+                                inputProps={{
+                                    placeholder: 'Start typing to search...',
+                                    value: excludedTableName,
+                                    onChange: (event, { newValue }) => {
+                                        setExcludedTableName(newValue);
+                                    },
+                                }}
                             />
                         </div>
                     </Stack>
                 </Col>
                 <Col>
                     <Stack gap={3}>
-                        
+                        <div className="form-input">
+                            <Form.Label>Ngày Bắt đầu</Form.Label>
+                            <Form.Control required type="date" onChange={updateStartDate}/>
+                        </div>
+                        <div className="form-input">
+                            <Form.Label>Ngày Kết thúc</Form.Label>
+                            <Form.Control required type="date" onChange={updateEndDate}/>
+                        </div>
+                        <div className="form-input">
+                            <Form.Label>Choose file</Form.Label>
+                            <Form.Control type="file" required  onChange={handleFileInputChange} />
+                        </div>
+                        <div className="form-input">
+                            <Form.Label>Select separation symbol</Form.Label>
+                            <Form.Select required onChange={handleSeparationSymbolChange}>
+                                {separationSymbols.map(type => <option>{type}</option>)}
+                            </Form.Select>
+                        </div>
                     </Stack>
                 </Col>
-            </Row>
-        <div class="container text-center" style={{marginTop: "20px"}}>
-            <input class="form-control" type="file" id="formFileMultiple" onChange={handleFileChange} multiple></input>
-        </div>
-        <div className='tables-grid'>
-            <Row>
-                <Col>
-                    <Table striped bordered>
-                            <tbody>
-                                <tr>
-                                    <td className='table-title' colSpan={2}>Tên File Để Thực hiện</td>
-                                </tr>
-                                    {
-                                        fileNames.map(x => <tr><td><span><em>{x}</em></span></td></tr>)
-                                    }
-                            </tbody>
-                        </Table>
-                    </Col>
-                    <Col>
-                        <Table striped bordered>
-                            <tbody>
-                                <tr>
-                                    <td className='table-title' colSpan={2}>Separate</td>
-                                </tr>
-                                <tr>
-                                    <td>Test; Test 2; Test 3</td>
-                                </tr>
-                            </tbody>
-                        </Table>
-                    </Col>
-
-                </Row>
-        </div>    
+            </Row>  
         <div class="container">
             <div className="row">
                 <div className="col text-end"><button type="button" class="btn btn-warning" onClick={refreshPage}>Nhập Lại</button></div>
-
                 {/* Tại bước này tạo một onclick => Khi bấm vào sẽ add tên các file vào một list mới để bắt đầu phân tích  */}
                 <div className="col"><button type="button" class="btn btn-primary">Thực Hiện</button></div>
             </div>
@@ -235,7 +281,7 @@ export default function ValidationDashBoard() {
                 <Table striped bordered>
                     <tbody>
                         <tr>
-                            <td className='table-title' colSpan={2}>Biểu đồ Tròn</td>
+                            <td className='table-title' colSpan={2}>Thống kê bảng tốt / cần kiểm tra</td>
                         </tr>
                         <tr>
                             <td><Pie data={dataForPie}/></td>
